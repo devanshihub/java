6e6f5aa133e88c8c65aaaff314aac4516ef8d2b8
devanshihub <devanshimehrotra2024@gmail.com>
2024-08-25 19:36:51 +0530

    Update positive_negative_no.java

--- Conditional Statements/positive_negative_no.java
+++ Conditional Statements/positive_negative_no.java
@@ -1,6 +1,6 @@
 // Write a Java program to get a number from the user and print whether it is positive or negative.
 import java.util.*;
- class classmain
+public class  positive_negative_no
 {
     public static void main(String k[])
     {
